1e173ee6e869206d007368fbbbd3ef035e41da87
Christopher Terrano <mterrano1@yahoo.com>
2023-02-27 09:50:58 -0600

    add routes to app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+      <Routes>
+      </Routes>
+  );
+}
+
+export default App;
