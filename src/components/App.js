f44a9e1177624a13c50bf487e39f80c70fdea1e9
Christopher Terrano <mterrano1@yahoo.com>
2023-02-27 10:08:22 -0600

    create home page

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { UserProvider } from './UserContext';
 import NavBar from './NavBar';
+import Home from '../pages/Home';
 
 function App() {
   return (
     <UserProvider>
     <NavBar />
     <Routes>
+      <Route exact path='/' element={<Home />} />
     </Routes>
   </UserProvider>
   );
